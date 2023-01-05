7f5ad8acb28f3c35097ed19b1fa8261a59d66b5e
Evans Sese <Sese_evans@yahoo.com>
2023-01-05 21:27:29 +0300

    Add files via upload
    fix:Improved isPrime functionality to capture 2,3 as prime numbers.

--- Prime.java
+++ Prime.java
@@ -5,14 +5,18 @@ public class Prime {
 
     static int isPrime(int number) {
         int isPrime = 0;
-        for (int divider = 2; 2 * divider < number; divider++) {
-            if (number % divider == 0) {
-                isPrime = 0;
-                break;
-            } else {
-                isPrime = 1;
+        if (number > 1) {
+            for (int divider = 2; divider <= number; divider++) {
+                if (number % divider == 0 && divider != number) {
+                    return isPrime;
+                } else {
+                    isPrime = 1;
+                }
             }
+            return isPrime;
+        } else {
+            return isPrime;
         }
-        return isPrime;
+
     }
 }
