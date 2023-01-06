4d808a07be6171f5891e79059107ee3f02fb1b55
Evans Sese <evanssese166@gmail.com>
2023-01-06 18:49:08 +0300

    Fix: Improved isTwin method

--- Twin.java
+++ Twin.java
@@ -1,22 +1,66 @@
 public class Twin {
     public static void main(String[] args) {
-        System.out.print(isPrime(67));
+        int[] arr;
+        arr = new int[] { 3, 5, 8, 10, 27 };
+        System.out.println(isTwin(arr));
     }
 
     public static int isPrime(int number) {
-        int flag = 1;
-        // for loop to find factors of the given number
-        for (int divider = 2; divider * 2 < number; divider++) {
-            // check if the divider is a factor of the number
-            if (number % divider == 0) {
-                flag = 0;
-                return flag;
+        int isPrime = 0;
+        if (number > 1) {
+            for (int divider = 2; divider <= number; divider++) {
+                if (number % divider == 0 && divider != number) {
+                    isPrime = 0;
+                    return isPrime;
+                } else {
+                    isPrime = 1;
+                }
             }
+            return isPrime;
+        } else {
+            return isPrime;
         }
-        return flag;
     }
 
-    // public static int isTwin(int[] arr){
+    public static int isTwin(int[] arr) {
+        // flag to hold either 0 or 1;
+        int twinFlag = 0;
+        // loop through the array to find prime numbers
+        for (int i = 0; i < arr.length; i++) {
+            int number = arr[i];
+            // check if the current number is prime
+            if (isPrime(number) == 1) {
+                // identify the adjacent numbers (expected twin)
+                int upperNumber = number + 2;
+                int lowerNumber = number - 2;
 
-    // }
+                // check if upper number is prime
+                if (isPrime(upperNumber) == 1) {
+                    // check if the number is in the array
+                    for (int j = 0; j < arr.length; j++) {
+                        if (arr[j] == upperNumber) {
+                            twinFlag = 1;
+                        } else {
+                            twinFlag = 0;
+                        }
+                    }
+                }
+
+                // check if lower number is prime
+                if (isPrime(lowerNumber) == 1) {
+                    // check if the number is in the array
+                    for (int j = 0; j < arr.length; j++) {
+                        if (arr[j] == lowerNumber) {
+                            twinFlag = 1;
+                        } else {
+                            twinFlag = 0;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        return twinFlag;
+    }
 }
