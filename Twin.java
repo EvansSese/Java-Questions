9ae3348e4133f21554249bfd56e5e55bcb8a56f0
Evans Sese <Sese_evans@yahoo.com>
2023-01-10 15:44:43 +0000

    Refactor: Refactoring isTwin method

--- Twin.java
+++ Twin.java
@@ -1,49 +1,39 @@
 public class Twin {
     public static void main(String[] args) {
         int[] arr;
-        arr = new int[] { 13, 14, 15, 3, 5 };
+        arr = new int[] { 11, 9, 12, 13, 23 };
         System.out.println(isTwin(arr));
     }
 
-    public static int isPrime(int number) {
-        int isPrime = 0;
-        if (number > 1) {
-            for (int divider = 2; divider <= number; divider++) {
-                if (number % divider == 0 && divider != number) {
-                    isPrime = 0;
-                    return isPrime;
-                } else {
-                    isPrime = 1;
-                }
-            }
-            return isPrime;
-        } else {
-            return isPrime;
-        }
-    }
-
     public static int isTwin(int[] arr) {
-        // flag to hold either 0 or 1;
-        int twinFlag = 0;
-        // loop through the array to find prime numbers
+        boolean hasTwins;
         for (int i = 0; i < arr.length; i++) {
-            int number = arr[i];
-            // check if the current number is prime
-            if (isPrime(number) == 1) {
-                // identify the adjacent numbers (expected twin)
-                int upperNumber = number + 2;
-                int lowerNumber = number - 2;
-
-                // check if upper number is prime
+            if (isPrime(arr[i])) {
+                hasTwins = false;
                 for (int j = 0; j < arr.length; j++) {
-                    if ((arr[j] == upperNumber && isPrime(upperNumber)==1) || (arr[j] == lowerNumber && isPrime(lowerNumber)==1) ) {
-                        twinFlag = 1;
+                    if (isPrime(arr[j]) && Math.abs(arr[i] - arr[j]) == 2) {
+                        hasTwins = true;
                         break;
                     }
                 }
+                if (!hasTwins) {
+                    // prime number does not have a twin in the array
+                    return 0;
+                }
             }
         }
-
-        return twinFlag;
+        return 1;
+    }
+    
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
