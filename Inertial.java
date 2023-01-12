fb2f86ae05abfcf2298d8e48b20df37239519981
Evans Sese <Sese_evans@yahoo.com>
2023-01-12 13:57:23 +0000

    Refactor: Refactored isInertial method

--- Inertial.java
+++ Inertial.java
@@ -1,17 +1,16 @@
 public class Inertial {
     public static void main(String[] args) {
-        int[] a = { 1,2,3,4 };
+        int[] a = { 8, 4, 20, 9, 2, 11 };
         System.out.println(isInertial(a));
 
-        // int[] b = { 2, 3, 5, 7 };
-        // System.out.println(isInertial(b));
+        int[] b = { 12, 11, 4, 9, 2, 3, 10 };
+        System.out.println(isInertial(b));
     }
 
     static int isInertial(int[] a) {
         boolean hasOdd = false; //default value is false
+        boolean oddIsGreater = true; //default value is true
         int maxVal = Integer.MIN_VALUE;
-        int evenVal = Integer.MIN_VALUE;
-        int prevEvenVal= Integer.MIN_VALUE;
         int leastOddVal= Integer.MAX_VALUE;
 
         //loop through the array to check for condition 1,2 and evaluate for condition 3
@@ -23,19 +22,19 @@ public class Inertial {
                 }
             }
 
-            if(a[i]%2==0){ //check if its even
-                if(a[i]>evenVal){
-                    evenVal = a[i];
-                }
-            }
-
-            if(a[i]>maxVal){
+            if(a[i]>maxVal){ //check for max val
                 maxVal = a[i]; //set maxVal
             }
 
         }
 
-        if((hasOdd) && (maxVal % 2 ==0) && (leastOddVal>prevEvenVal)){ //check if all 3 conditions are true
+        for(int j=0; j<a.length;j++){ //loop to check if each odd value is greater than every even except maxVal
+            if(a[j]%2==0 && (leastOddVal<a[j]) && (a[j]!=maxVal)){
+                oddIsGreater = false;
+            }
+        }
+
+        if((hasOdd) && (maxVal % 2 ==0) && (oddIsGreater)){ //check if all 3 conditions are true
             return 1;
         } else{
             return 0;
