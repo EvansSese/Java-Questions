1fed0c71cecd989e04f08d9e5e33aa281f5bfb36
Evans Sese <Sese_evans@yahoo.com>
2023-01-13 15:34:06 +0000

    Fix: Improved findPorcupineNumber method

--- Porcupine.java
+++ Porcupine.java
@@ -1,6 +1,6 @@
 public class Porcupine {
     public static void main(String[] args) {
-        System.out.println(findPorcupineNumber(1));
+        System.out.println(findPorcupineNumber(409));
     }
 
     public static int isPrime(int number) {
@@ -21,25 +21,27 @@ public class Porcupine {
     }
 
     public static int findPorcupineNumber(int n){
-        int porcupineNum = 0;
-        boolean isPorcupine = false;
-        n++;
-        while(n < Integer.MAX_VALUE){
-            if(isPorcupine){
-                if(isPrime(n)==1){
-                    break;
-                }else{
-                    isPorcupine = false;
-                }
-            }else{
-                if(isPrime(n)==1){
-                    if(n%10==9){
-                        isPorcupine = true;
-                        porcupineNum = n;
+        int porcupineNum = 0; //initialize porcupine number as 0;
+        boolean checkPorcupine = true; //set checking condtion to true;
+        while(checkPorcupine){
+            n++; //check next number after given number
+            if(isPrime(n)==1){ //pass prime test
+                if(n%10==9){ //has 9 at the end
+                    porcupineNum = n; //set n as the porcupine number
+                    boolean checkNextPorcupine = true; //set condition for checking next prime number
+                    while(checkNextPorcupine){
+                        n++;
+                        if(isPrime(n)==1){ //pass isPrime test
+                            if(n%10==9){ //has 9 at the end
+                                checkNextPorcupine = false; //terminate the nested while loop
+                                checkPorcupine = false; //terminate the outer while loop
+                            }else{
+                                checkNextPorcupine = false; //terminate the inner while loop and continue with the outer while loop
+                            }
+                        }
                     }
                 }
             }
-
         }
         return porcupineNum;
     }
