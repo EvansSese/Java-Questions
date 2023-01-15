8472cd8dfec519b2499197709c1c3f9c3f4c13fa
Evans Sese <Sese_evans@yahoo.com>
2023-01-15 15:28:36 +0000

    Docs: Added comments in the PerfectSquare code

--- PerfectSquare.java
+++ PerfectSquare.java
@@ -5,17 +5,17 @@ class PerfectSquare {
     }
 
     public static int nextPerfectSquare(int n) {
-        int nextPerfectSquare = 0;
+        int nextPerfectSquare = 0; //initialize nextPerfectSquare as 0
 
-        if (n < 0) {
+        if (n < 0) { //if below 0, return 0
             nextPerfectSquare = 0;
-        } else if (n == 0) {
+        } else if (n == 0) { //if n is 0, return 1
             nextPerfectSquare = 1;
-        } else {
+        } else { //start loop at 1 and end at n
             for (int i = 1; i < n; i++) {
                 nextPerfectSquare = i * i;
-                if (n < nextPerfectSquare) {
-                    return nextPerfectSquare;
+                if (n < nextPerfectSquare) { //check if the nextPerfectSquare is greater than n
+                    return nextPerfectSquare; //break loop as soon as we get the perfect square
                 }
             }
         }
