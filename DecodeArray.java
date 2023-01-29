8694c56c4b8fe12e4882b619cbd2e35e7de17b69
EvansSese <Sese_evans@yahoo.com>
2023-01-29 13:16:26 +0300

    Doc: Added comments in the DecodeArray class

--- DecodeArray.java
+++ DecodeArray.java
@@ -6,22 +6,23 @@ public class DecodeArray {
 
     public static int decodeArray(int[] a) {
         String s = Integer.toString(0);
-        for (int i = 1; i < a.length; i++) { // start at second element, so that we can fetch the first and find the
-                                             // difference
+        for (int i = 1; i < a.length; i++) { // start at second element, so that we can get the first and find the
+                                             // diff
             int prev_element = a[i - 1];
             int current_element = a[i];
-            int element_difference = Math.abs(prev_element - current_element);
+            int element_difference = Math.abs(prev_element - current_element); // abs difference between the two
+                                                                               // elements
 
-            String num_string = Integer.toString(element_difference);
-            s = s + num_string;
+            String num_string = Integer.toString(element_difference); // convert to string so that we can concatenate
+            s = s + num_string; // concatenate the string
         }
 
-        int encodedNum = Integer.parseInt(s);
+        int encodedNum = Integer.parseInt(s); // convert back to integer
 
         if (a[0] < 0) {
-            encodedNum = -1 * encodedNum;
+            encodedNum = -1 * encodedNum; // check if first element is negative, then encoded number is also negative
         }
 
-        return encodedNum;
+        return encodedNum; // return encoded number
     }
 }
