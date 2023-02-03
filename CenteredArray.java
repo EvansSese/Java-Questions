90b84f2e98d542f1bd07afffeb71d0072dba8652
EvansSese <sese_evans@yahoo.com>
2023-02-04 00:58:32 +0300

    Added isCentered method

--- CenteredArray.java
+++ CenteredArray.java
@@ -7,8 +7,9 @@ public class CenteredArray {
     public static int isCentered(int[] a) {
         // check if the length of array is odd
         if (a.length % 2 != 0) {
-            int centerIndex = a.length / 2;
+            int centerIndex = a.length / 2; // get mid index
             for (int i = 0; i < a.length; i++) {
+                // check if any item is less than mid item, except the mid item itself
                 if ((i != centerIndex) && a[i] <= a[centerIndex]) {
                     return 0;
                 }
