8147153dcb48a7a463a3df30276fa29892047574
EvansSese <sese_evans@yahoo.com>
2023-02-03 22:30:22 +0300

    Improved isNice method

--- Nice.java
+++ Nice.java
@@ -1,27 +1,28 @@
 public class Nice {
     public static void main(String[] args) {
-        int[] a = { 1, -1, 1 };
+        int[] a = { 3, 4, 5, 7 };
         System.out.println(isNice(a));
     }
 
     public static int isNice(int[] a) {
-        // loop through the elements and find their adjacent
+        // loop through the array
         for (int i = 0; i < a.length; i++) {
-            // initialize has adj boolean
-            boolean hasAdj = false;
-
-            for (int j = 0; j < a.length; j++) {
-                if (a[j] == a[i] + 1 || a[j] == a[i] - 1) {
-                    hasAdj = true;
-                    break;
-                }
-            }
-
-            // check if the element has no adjacent
-            if (!hasAdj) {
+            if (isAvailable(a, a[i]) == 0) {
                 return 0;
             }
         }
         return 1;
+    }
+
+    public static int isAvailable(int[] a, int n) {
+        // loop through to see if the adjacent values are available
+        for (int j = 0; j < a.length; j++) {
+            if (a[j] == n - 1 || a[j] == n + 1) {
+                // value is available
+                return 1;
+            }
+        }
+        // value is not available
+        return 0;
     }
 }
